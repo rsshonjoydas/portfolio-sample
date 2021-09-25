bda191b9717ba6120cba4af16a32cd63c6b7e649
rsshonjoydas <rsshonjoydas@gmail.com>
2021-09-26 03:52:40 +0600

    contact page update material-ui

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,27 +1,67 @@
-const Contact = () => {
-  return (
-    <section className="contact section" id="contact">
-      <h2 className="section-title">Contact</h2>
+import { KeyboardArrowRight } from "@mui/icons-material";
+import { Button, Container, Grid, TextField } from "@mui/material";
+import { makeStyles } from "@mui/styles";
 
-      <div className="contact__container bd-grid">
-        <form action="" className="contact__form">
-          <input type="text" placeholder="Name" className="contact__input" />
-          <input type="mail" placeholder="Email" className="contact__input" />
-          <textarea
-            name=""
-            id=""
-            cols="0"
-            rows="10"
-            className="contact__input"
-          ></textarea>
-          <input
-            type="button"
-            value="Enviar"
-            className="contact__button button"
+const useStyles = makeStyles({
+  field: {
+    marginTop: 50,
+    display: "flex",
+    maxWidth: 500,
+  },
+});
+
+const Contact = () => {
+  const classes = useStyles();
+  return (
+    <Container className="section">
+      <h2 className="section-title">Create a New Note</h2>
+
+      <form noValidate autoComplete="off">
+        <Grid
+          container
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <TextField
+            className={classes.field}
+            label="Name"
+            variant="outlined"
+            color="primary"
+            fullWidth
+            required
           />
-        </form>
-      </div>
-    </section>
+          <TextField
+            className={classes.field}
+            label="Email"
+            variant="outlined"
+            color="primary"
+            fullWidth
+            required
+          />
+          <TextField
+            className={classes.field}
+            label="What's on your mind?"
+            variant="outlined"
+            color="primary"
+            multiline
+            rows={4}
+            fullWidth
+            required
+          />
+          <Button
+            className={`button ${classes.field}`}
+            type="submit"
+            color="primary"
+            variant="contained"
+            endIcon={<KeyboardArrowRight />}
+            href="#"
+          >
+            Submit
+          </Button>
+        </Grid>
+      </form>
+    </Container>
   );
 };
 
