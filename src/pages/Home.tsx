2d7be18d824cfece26f5aebb77fc976f44135205
rsshonjoydas <rsshonjoydas@gmail.com>
2021-09-25 16:55:41 +0600

    skill section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import Header from "../components/Header";
+import Skill from "../components/Skill";
 import About from "./About";
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
     <>
       <Header />
       <About />
+      <Skill />
     </>
   );
 };
