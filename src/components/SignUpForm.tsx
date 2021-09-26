e3a6714e231d7733a73ac115eb93d76379b3777f
rsshonjoydas <rsshonjoydas@gmail.com>
2021-09-26 21:29:35 +0600

    complete sign in

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -2,36 +2,36 @@ import { Link } from "react-router-dom";
 import classes from "../styles/login.module.css";
 import SocialAuth from "./SocialAuth";
 
-const LoginForm = () => {
+const SignUpForm = () => {
   return (
     <div className={`section ${classes.loginForm}`}>
       <div className={classes.loginBox}>
-        <h2 className="section-title">Login</h2>
+        <h2 className="section-title">Sign Up</h2>
         <form>
           {/* //? input box */}
           <div className={classes.userBox}>
             <input type="text" name="" required />
-            <label>Username</label>
+            <label>Name</label>
+          </div>
+          <div className={classes.userBox}>
+            <input type="email" name="" required />
+            <label>Email</label>
           </div>
           <div className={classes.userBox}>
             <input type="password" name="" required />
             <label>Password</label>
           </div>
           <div className={classes.link}>
-            {/*//? forget password */}
-            <Link className={classes.forgetPass} to="{#}">
-              Forget your password?
-            </Link>
             {/* //? social sign up login */}
             <p className={classes.socialText}>
-              Or Sign In with social platforms
+              Or Sign Up with social platforms
             </p>
             <SocialAuth />
             {/* //? sign up link */}
-            <Link to="{#}">Don't have an account?</Link>{" "}
+            <Link to="{#}">Already have an account?</Link>{" "}
             <span className={classes.or}>Or </span>
-            <Link to="/signup" className={classes.signupText}>
-              Sign Up
+            <Link to="/login" className={classes.signupText}>
+              Sign In
             </Link>
           </div>
           {/* //? submit button */}
@@ -48,4 +48,4 @@ const LoginForm = () => {
   );
 };
 
-export default LoginForm;
+export default SignUpForm;
